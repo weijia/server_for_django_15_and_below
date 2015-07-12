795d5014ad676f35c9aeb669e34394597158a903
Richard <richardwangwang@gmail.com>
2015-07-12 12:12:06 +0800

    Update djangoautoconf. Code refine.

--- server_base_packages/others/extra_settings/settings.py
+++ server_base_packages/others/extra_settings/settings.py
@@ -3,14 +3,14 @@ INSTALLED_APPS += (
     'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.admindocs',
-    #'south',  # Do not work in SAE
-    #'mptt',
-    #'treenav',
-    #'background_task',
-    #'django_cron',  # Do not work in SAE
-    #'jquery_ui',
-    #'provider',
-    #'provider.oauth2',
+    # 'south',  # Do not work in SAE
+    # 'mptt',
+    # 'treenav',
+    # 'background_task',
+    # 'django_cron',  # Do not work in SAE
+    # 'jquery_ui',
+    # 'provider',
+    # 'provider.oauth2',
     'webmanager',
 )
 
